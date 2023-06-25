184ba56ca12291aecda6f26798769a7610b02673
Dayglow0926 <dayglow0926@gmail.com>
2023-06-25 21:11:48 +0900

    feat: awesome 스크립트 코드 및 layout 스타일 추가

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,22 @@
+import Script from "next/script";
 import NavBar from "./NavBar";
 
 export default function Layout({ children }) {
   return (
     <>
       <NavBar />
-      <div></div>
+      <div className="layout">{children}</div>
+      <Script
+        src="https://kit.fontawesome.com/a4852596ef.js"
+        crossorigin="anonymous"
+      ></Script>
+      <style jsx>{`
+        .layout {
+          width: 100%;
+          display: flex;
+          justify-content: center;
+        }
+      `}</style>
     </>
   );
 }
